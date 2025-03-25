7ea50d5291ad724808f34fa4d86f086d76088273
Pacoletta <carlosmvale1@gmail.com>
2025-03-25 20:28:32 -0300

    Update pixRoutes.ts

--- src/routes/pixRoutes.ts
+++ src/routes/pixRoutes.ts
@@ -3,7 +3,7 @@ import { PixController } from '../controllers/pixController';
 
 const router = Router();
 
-router.post('/api/gerar_pix', PixController.gerarPix);
+router.post('/gerar_pix', PixController.gerarPix);
 router.get('/health', PixController.health);
 
 export default router;
